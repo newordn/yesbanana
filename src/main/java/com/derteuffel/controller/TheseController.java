e98b9f15e9705cbce1dc3d36476ffea382901e96
derteuffel <derteuffel0@gmail.com>
2018-11-13 15:53:01 +0100

    updating theseController remove library fuction and in app properties change meil address sending to newordn@gmail.com

--- src/main/java/com/derteuffel/controller/TheseController.java
+++ src/main/java/com/derteuffel/controller/TheseController.java
@@ -294,12 +294,7 @@ public class TheseController {
         return "these/these1";
     }
 
-    @GetMapping("/library/{theseId}")
-    public String getlibrary(Model model, @PathVariable Long theseId){
-        Optional<These> optional= theseRepository.findById(theseId);
-        model.addAttribute("these2",optional.get());
-        return "these/these2";
-    }
+
 
 }
 
