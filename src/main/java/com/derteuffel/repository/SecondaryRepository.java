03e8a84e8d7835239664ecf22d7e20334a42639d
derteuffel <derteuffel0@gmail.com>
2019-04-25 21:23:29 +0200

    adding secondary module

--- src/main/java/com/derteuffel/repository/SecondaryRepository.java
+++ src/main/java/com/derteuffel/repository/SecondaryRepository.java
@@ -1,6 +1,8 @@
 package com.derteuffel.repository;
 
+import com.derteuffel.data.Education;
 import com.derteuffel.data.Secondary;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +14,16 @@ import java.util.List;
  * Created by derteuffel on 27/12/2018.
  */
 @Repository
-public interface SecondaryRepository extends JpaRepository<Secondary, Long> {
+public interface SecondaryRepository extends JpaRepository<Education, Long> {
 
     @Query("select s from Secondary as s join s.region sr where sr.regionId=:id order by s.educationId desc")
     List<Secondary> findAllByRegion(@Param("id") Long regionId);
+
+    Secondary findByTitle(String title);
+
+    List<Secondary> findAllByStatus(Boolean status);
+    List<Secondary> findAllByOrderByEducationIdDesc();
+    List<Secondary> findFirst12ByType(String type, Sort sort);
+    List<Secondary> findAllByTypeOrderByEducationIdDesc(String type);
 }
 
