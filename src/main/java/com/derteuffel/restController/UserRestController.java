95a7acb346558341d2605a91086146c0dd894192
derteuffel <derteuffel0@gmail.com>
2019-06-05 01:03:34 +0200

    adding register function in userRestController

--- src/main/java/com/derteuffel/restController/UserRestController.java
+++ src/main/java/com/derteuffel/restController/UserRestController.java
@@ -2,9 +2,11 @@ package com.derteuffel.restController;
 
 import com.derteuffel.data.User;
 import com.derteuffel.repository.UserRepository;
+import com.derteuffel.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -24,8 +26,46 @@ public class UserRestController {
     public Collection<User> list(){
         return userRepository.findAllByOrderByUserIdDesc();
     }
+    private String validate_url="yesbanana.org/validate/";
 
     //users visitor management function
+    @PostMapping(value = "/mobile/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
+    public Map<String, String> restRegistration(@RequestBody Map<String,String> object){
+       Map map=new HashMap<String,String>();
+
+        User user= userRepository.findByName(object.get("login"));
+        if (user!= null){
+            map.put("status","Cet utilisateur existe deja, veuillez changer de login");
+            return map;
+        }
+
+        User user1= userRepository.findByEmail(object.get("email"));
+        if (user1!= null){
+            map.put("status","Cet utilisateur existe deja, veuillez changer d'adresse email");
+            return map;
+        }
+        User user2=new User();
+        user2.setName(object.get("login"));
+        user2.setEmail(object.get("email"));
+        user2.setPassword(bCryptPasswordEncoder.encode(object.get("password")));
+        user2.setCountry(object.get("country"));
+        user2.setRegion(object.get("region"));
+        user2.setNumber(object.get("number"));
+        user2.setPar_mobile(false);
+        user2.setStatus(true);
+        userRepository.save(user2);
+        MailService mailService = new MailService();
+        mailService.sendSimpleMessage(
+                "solutioneducationafrique@gmail.com",
+                "YesBanana: Notification Inscription d'un utilisateur a partir de l'application mobile",
+                "L'utilisateur " + user2.getName() + " dont l'email est " +
+                        user2.getEmail()+ "  Vient de s'inscrire " +
+                        "sur la plateforme YesBanana.");
+
+        map.put("status","inscription reussit");
+        return map;
+
+    }
 
     @PostMapping(value = "/mobile/login",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String,String> restLogin( @RequestBody Map<String,String> object){
