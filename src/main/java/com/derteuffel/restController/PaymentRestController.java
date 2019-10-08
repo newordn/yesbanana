432d5c090209b13c2af7ff9bd409f1777a159cf2
Neword <newordn@gmail.com>
2019-10-08 21:33:43 +0100

    Completing stripe rest integration

--- src/main/java/com/derteuffel/restController/PaymentRestController.java
+++ src/main/java/com/derteuffel/restController/PaymentRestController.java
@@ -1,8 +1,10 @@
 package com.derteuffel.restController;
 
+import com.derteuffel.service.StripeService;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,23 +15,28 @@ import java.util.Map;
 
 @RestController
 public class PaymentRestController {
+    @Autowired
+    private StripeService paymentsService;
 
-    @PostMapping(value = "/do_payment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
-    public Map<String, Boolean > doPayment(@RequestBody Map<String,String> object)
+
+    @PostMapping(value = "/mobile/do_payment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
+    public Map<String, String > doPayment(@RequestBody Map<String,String> object)
     {
-        System.out.println(object);
-        Map<String,Boolean> map = new HashMap<String,Boolean>();
-        Stripe.apiKey= "sk_test_XQayGouNu0oTFSRnX6lFzkru00GjI4eZlA";
+        System.out.println("Payment object" + object);
+        Map<String,String> map = new HashMap<String,String>();
         Map<String,Object> chargeMap = new HashMap<String,Object>();
         chargeMap.put("amount",object.get("amount"));
         chargeMap.put("currency",object.get("currency"));
         chargeMap.put("source",object.get("token"));
         chargeMap.put("description",object.get("description"));
         try{
-            Charge charge = Charge.create(chargeMap);
+            Charge charge = paymentsService.charge(chargeMap);
+
+            map.put("status","true");
             System.out.println(charge);
         }catch(StripeException e)
         {
+            map.put("status","false");
             e.printStackTrace();
         }
         return map;
